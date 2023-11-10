15bbc9a8012e2580233913f75507c4c92cbd9022
Frans Holwerda <fholwerd@f1r4s20.codam.nl>
2023-11-10 13:27:44 +0100

    completed 2017 day 06

---
+++ 2017/Day06/star_two.py
@@ -0,0 +1,31 @@
+lines = []
+with open("input.txt", 'r') as fd:
+	lines = fd.readlines()
+
+array = []
+for line in lines:
+	for number in line.split():
+		array.append(int(number))
+
+redistribution_cycle = 0
+seen_arrays = []
+done = False
+while not done:
+	seen_arrays.append(array.copy())
+	bank = max(array)
+	cursor = array.index(bank)
+	array[cursor] = 0
+	cursor += 1
+	while bank > 0:
+		if cursor >= len(array):
+			cursor = 0
+		array[cursor] += 1
+		bank -= 1
+		cursor += 1
+	redistribution_cycle += 1
+	if array in seen_arrays:
+		done = True
+
+first_array_occurence = seen_arrays.index(array)
+cycles_since_first_occurrence = redistribution_cycle - (first_array_occurence)
+print(cycles_since_first_occurrence)
